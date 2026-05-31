06c3283801e85b300403327ec1cc78d263df8502
PhamNhanHau <phamnhanhau23042005@gmail.com>
2026-05-31 11:25:21 +0700

    Sua loi duong dan

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,10 +3,7 @@ using System.Data;
 using System.Windows;
 
 namespace DoAnCSharp_WPF
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+{ 
     public partial class App : Application
     {
     }
