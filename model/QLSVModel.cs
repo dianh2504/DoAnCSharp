2c0d6668673de001e66cae3977e35f47296e3364
PhamNhanHau <phamnhanhau23042005@gmail.com>
2026-05-31 10:38:07 +0700

    Fix lai vi tri thu muc cho gitinore hoat dong

---
+++ model/QLSVModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoAnCSharp_WPF.Models
+{
+    internal class QLSVModel
+    {
+        public List<SinhVien> DsSinhVien { get; set; } = new List<SinhVien>();
+        private string chucNang = "";
+        private bool trangThaiSQL = false;
+
+        public string ChucNang
+        {
+            get { return chucNang; }
+            set { chucNang = value; }
+        }
+        public bool TrangThaiSQL
+        {
+            get { return trangThaiSQL; }
+            set { trangThaiSQL = value; }
+        }
+
+        //Constructor
+        public QLSVModel(List<SinhVien> dsSinhVien)
+        {
+            this.DsSinhVien = dsSinhVien;
+            this.chucNang = "";
+            this.trangThaiSQL = false;
+        }
+        public QLSVModel()
+        {
+            this.DsSinhVien = new List<SinhVien>();
+        }
+
+        //Cac ham chuc nang tuong tac voi List
+        public void themSinhVien(SinhVien sv)
+        {
+            this.DsSinhVien.Add(sv);
+        }
+        public void chinhSuaSinhVien(SinhVien sv)
+        {
+            this.DsSinhVien.Remove(sv);
+            this.DsSinhVien.Add(sv);
+        }
+        public void xoaSinhVien(SinhVien sv)
+        {
+            this.DsSinhVien.Remove(sv);
+        }
+        //Cac ham tuong tac voi listener va view
+        public SinhVien getSinhVienTuMaSinhVien(int maSinhVien)
+        {
+            foreach (SinhVien sv in this.DsSinhVien)
+            {
+                if (sv.MaSinhVien == maSinhVien)
+                {
+                    return sv;
+                }
+            }
+            return null;
+        }
+
+        public void kiemTraTrungMaSinhVien(int maSinhVien)
+        {
+            foreach (SinhVien sv in this.DsSinhVien)
+            {
+                if (maSinhVien == sv.MaSinhVien)
+                {
+                    throw new Exception("Mã sinh viên đã tồn tại trong hệ thống!");
+                }
+            }
+        }
+    }
+}
---
+++ model/Tinh.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Xml.Linq;
+namespace DoAnCSharp_WPF.Models
+{
+    public class Tinh
+    {
+        private int maTinh;
+        public int MaTinh  
+        {
+            get
+            {
+                return maTinh;
+            }
+            set
+            {
+                if (value > 0 && value <35)
+                {
+                    maTinh = value;
+                } else
+                {
+                    throw new ArgumentException("Mã tỉnh phải trong phạm vi từ 1 đến 34");
+                }
+            }
+        }
+        private string tenTinh;
+        public string TenTinh
+        {
+            get
+            {
+                return tenTinh;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    tenTinh = value;
+                } else
+                {
+                    throw new ArgumentException("Tên tỉnh không được để trống");
+                }
+            }
+        }
+        //Constructor rong va chuan
+        public Tinh()
+        {
+        }
+        public Tinh(int maTinh, string tenTinh)
+        {
+            this.MaTinh = maTinh;
+            this.TenTinh = tenTinh;
+        }
+        //toString
+        public override string ToString()
+        {
+            return $"{tenTinh}";
+        }
+        //Hashcode và Equals
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(maTinh, tenTinh);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            if (this == obj) 
+                return true;
+            Tinh other = (Tinh)obj;
+            return this.maTinh == other.MaTinh;
+        }
+        public static List<Tinh> getDSTinh()
+        {
+            String[] arr_tinh = {
+                "An Giang",
+                "Bắc Ninh",
+                "Cà Mau",
+                "Cao Bằng",
+                "Đắk Lắk",
+                "Điện Biên",
+                "Đồng Nai",
+                "Đồng Tháp",
+                "Gia Lai",
+                "Hà Tĩnh",
+                "Hưng Yên",
+                "Khánh Hòa",
+                "Lai Châu",
+                "Lạng Sơn",
+                "Lào Cai",
+                "Lâm Đồng",
+                "Nghệ An",
+                "Ninh Bình",
+                "Phú Thọ",
+                "Quảng Ngãi",
+                "Quảng Ninh",
+                "Quảng Trị",
+                "Sơn La",
+                "Tây Ninh",
+                "Thái Nguyên",
+                "Thanh Hóa",
+                "TP. Cần Thơ",
+                "TP. Đà Nẵng",
+                "TP. Hà Nội",
+                "TP. Hải Phòng",
+                "TP. Hồ Chí Minh",
+                "TP. Huế",
+                "Tuyên Quang",
+                "Vĩnh Long"};
+            List<Tinh> listTinh = new List<Tinh>();
+
+            int i = 1;
+            foreach (String tenTinh in arr_tinh)
+            {
+                Tinh t = new Tinh(i, tenTinh);
+                listTinh.Add(t);
+                i++;
+            }
+            return listTinh;
+        }
+        //Ham lay object tinh
+        public static Tinh getTinhById(int queQuan)
+        {
+            return Tinh.getDSTinh().FirstOrDefault(t => t.maTinh == queQuan);
+        }
+        public static Tinh getTinhByTen(string tenTinh)
+        {
+            return Tinh.getDSTinh().FirstOrDefault(t => t.tenTinh == tenTinh);
+        }
+    }
+}
